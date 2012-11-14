b6c517536b3c59104438d14d95e22c0366be6aa4
Engin Kurutepe <engin@kurutepe.com>
2012-11-14 14:44:15 +0100

    remove UIViewExt rootView

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -103,7 +103,7 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-	rootView = [[UIViewExt alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+	rootView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
 	rootView.autoresizingMask = UIViewAutoresizingFlexibleWidth + UIViewAutoresizingFlexibleHeight;
 	[rootView setBackgroundColor:[UIColor clearColor]];
 
